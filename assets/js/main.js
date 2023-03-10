e6496c515aefbb42e6fdc680a6ced4bc8a3cf0e3
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-10 15:45:59 +0100

    js for console output

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,14 @@
 //settiamo il valore del nostro index a 0
 //iniziamo il for < 100
 for (let index = 0; index <=  100; index++) {
-    console.log(index);
+    if (index % 3 == 0 && index % 5 == 0) { //se index è div per 3 e 5 allora stampiamo both
+       console.log('FizzBuzz'); 
+    } else if (index % 3 == 0) { //se index è div per 3 stampiamo fizz
+        console.log('Fizz');
+    } else if (index % 5 == 0) { //se index è div per 5 stampiamo buzz
+        console.log("Buzz");
+    } else { //altrimenti stampiamo il numero normale
+        console.log(index);
+    }
 }
 
